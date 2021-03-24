05fff9e4ebc02d2f454725f186eaec6d0e093d74
aya-blip <aya.rjiba@gmail.com>
2021-03-24 13:06:32 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
+
 import './App.css';
+import './style.css';
+import MyImageInSrc from "./imageInSrc.jpg";
+import React from 'react';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{border:"solid 1px black",maxWidth:'100vw'}}>  
+    <h1 className="title red">Aya Rjiba</h1>
+    <br/>
+    <img src="/imageInPublic.jpg" alt="myimage" />
+
+    <br/>
+    <img src={MyImageInSrc }/>
     </div>
+    <video width="750" height="500" controls >
+      <source src={"https://media.w3.org/2010/05/sintel/trailer_hd.mp4"} type="video/mp4"/>
+     </video>
+
+
+
+
+      
+      
+    
+    
+      
+    </div>
+
   );
 }
 
